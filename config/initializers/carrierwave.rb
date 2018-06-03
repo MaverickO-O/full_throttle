79388f902f2720e6b07cc972211cbe9f745f5e08
MaverickO-O <antoine.escarrat@gmail.com>
2018-06-03 15:06:38 +0900

    aws try

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-CarrierWave.configure do |config|
+
 
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
   # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
-      config.storage = :fog
+      
    
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
@@ -21,9 +21,9 @@ CarrierWave.configure do |config|
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
+  config.storage = :fog
   config.fog_directory  = ENV['S3_BUCKET']                                   # required
   #config.fog_public     = false                                                 # optional, defaults to true
   #config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
    end
   end
-end
