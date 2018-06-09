64352513a36e4fe9d7d0543b60564271573a6650
MaverickO-O <antoine.escarrat@gmail.com>
2018-06-09 10:51:42 +0900

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602053709) do
+ActiveRecord::Schema.define(version: 20180608232622) do
 
   create_table "bikes", force: :cascade do |t|
     t.string "bike_name"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20180602053709) do
     t.string "bikebrand1"
     t.string "bikename1"
     t.string "avatar"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
