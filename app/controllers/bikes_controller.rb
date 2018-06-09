48d1c98cf58f63d658e1646755f1904e25f37206
MaverickO-O <antoine.escarrat@gmail.com>
2018-06-09 15:46:29 +0900

    can delete bike post

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,5 @@
 class BikesController < ApplicationController
-  before_action :authenticate_user!, :except => [:index, :show]
+  #before_action :authenticate_user!, :except => [:index, :show]
 
   def index
     @bikes = Bike.all
