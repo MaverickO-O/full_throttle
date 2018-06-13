5ff114c16e6fcc9c6e603ebdb3f7c3a654dc84be
MaverickO-O <antoine.escarrat@gmail.com>
2018-06-13 22:15:29 +0900

    minimagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,9 +30,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  #version :thumb do
-  #  process resize_to_fit: [50, 50]
-  #end
+  version :thumb do
+    process :resize_to_fit [50, 50]
+  end
+
+  version :small do
+    process :resize_to_fit [150, 150]
+    #process :quality => 100
+  end
+
+  version :medium do
+    process :resize_to_fit [200, 200]
+    #process :quality => 100
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
