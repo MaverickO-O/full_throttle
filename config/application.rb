0b190f5fa32e645371a6cf8631ad2d1ba248ef58
MaverickO-O <antoine.escarrat@gmail.com>
2018-07-19 21:42:29 +0900

    encrypted_secrets = true

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 require "active_storage"
-#config.read_encrypted_secrets = true
+config.read_encrypted_secrets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
