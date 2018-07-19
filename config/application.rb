12a451359f2238f38b1fbb2f78cafa6860ad2a22
MaverickO-O <antoine.escarrat@gmail.com>
2018-07-19 21:47:38 +0900

    uncomment

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 require "active_storage"
-config.read_encrypted_secrets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +12,7 @@ module FullThrottle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.read_encrypted_secrets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
