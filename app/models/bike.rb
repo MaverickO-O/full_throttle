a707a0a2a30c9b936df2fa0538824280dca9e92c
MaverickO-O <antoine.escarrat@gmail.com>
2018-07-21 09:16:26 +0900

    avatar image

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,8 +4,4 @@ class Bike < ApplicationRecord
 	has_one_attached :picturetop
 	has_one_attached :picturebike
 
-
-	def bikethumbnail
-		return self.picturebike.variant(resize: '120x120').processed
-	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
   has_many :newsposts
   has_many :posts
   has_many :comments
-  mount_uploader :avatar, ImageUploader
+  #mount_uploader :avatar, ImageUploader
+  has_one_attached :avatar
 end
