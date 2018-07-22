8d0ee47fee38a8b7ee362348c6dfe41be67a89db
MaverickO-O <antoine.escarrat@gmail.com>
2018-07-22 17:44:07 +0900

    index bike add info

--- app/controllers/learns_controller.rb
+++ app/controllers/learns_controller.rb
@@ -1,5 +1,6 @@
 class LearnsController < ApplicationController
   before_action :set_learn, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index, :show]
 
   # GET /learns
   # GET /learns.json
