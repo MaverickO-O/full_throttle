0737b32eae9312068d82941e52dabeecf18a570f
MaverickO-O <antoine.escarrat@gmail.com>
2018-07-22 20:17:54 +0900

    clean menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :logopics
   resources :learns
   get 'users/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_234226) do
+ActiveRecord::Schema.define(version: 2018_07_22_100611) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -93,6 +93,18 @@ ActiveRecord::Schema.define(version: 2018_07_21_234226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "learntitle"
+  end
+
+  create_table "logopics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "newsposts", force: :cascade do |t|
