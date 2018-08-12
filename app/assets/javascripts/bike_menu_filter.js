3c1a3c1c12eb6019668d4ce2b1cc77dcb41e0357
MaverickO-O <antoine.escarrat@gmail.com>
2018-08-12 20:01:17 +0900

    new presenta index bike

--- app/assets/javascripts/bike_menu_filter.js
+++ app/assets/javascripts/bike_menu_filter.js
@@ -1,23 +1,23 @@
 function filter_function() {
   // Declare variables 
-  var input, filter, table, tr, td, i;
+  var input, filter, table, tr, td, i, ul, li;
   input_brand = document.getElementById("brand");
   input_type = document.getElementById("type");
   filter_brand = input_brand.value.toUpperCase();
   filter_type = input_type.value.toUpperCase();
-  table = document.getElementById("bike_table");
-  tr = table.getElementsByTagName("tr");
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
 console.log(input);
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td_brand = tr[i].getElementsByTagName("td")[0];//select colum 0
-    td_type = tr[i].getElementsByTagName("td")[3];//select colum 3
-    if (td_brand || td_type) {
-      if ((td_brand.innerHTML.toUpperCase().indexOf(filter_brand) > -1 || filter_brand == "-") && (td_type.innerHTML.toUpperCase().indexOf(filter_type) > -1 || filter_type == "-")) {
-        tr[i].style.display = "";
+  for (i = 0; i < li.length; i++) {
+    li_brand = li[i].getElementsByTagName("h3")[0];//select colum 0
+    li_type = li[i].getElementsByTagName("p")[0];//select colum 3
+    if (li_brand || li_type) {
+      if ((li_brand.innerHTML.toUpperCase().indexOf(filter_brand) > -1 || filter_brand == "-") && (li_type.innerHTML.toUpperCase().indexOf(filter_type) > -1 || filter_type == "-")) {
+        li[i].style.display = "";
       } else {
-        tr[i].style.display = "none";
+        li[i].style.display = "none";
       }
     } 
   }
